b5a6dccecf185f4e4c53896d4d0c6eedb5443b59
ikrom <ikrom.hasanbaev.27@gmail.com>
2024-08-29 14:18:11 +0300

    Product screen refactor

--- app/src/main/java/ru/ikrom/demo_ide_platform/ui/screens/ProductListScreen.kt
+++ app/src/main/java/ru/ikrom/demo_ide_platform/ui/screens/ProductListScreen.kt
@@ -1,26 +1,51 @@
 package ru.ikrom.demo_ide_platform.ui.screens
 
-import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ru.ikrom.demo_ide_platform.ui.components.ScreenTitleBar
 
 @Composable
 fun ProductListScreen(){
+
     Scaffold(
         topBar = {
             ScreenTitleBar("Список товаров")
         }
     ) { padding ->
-        LazyColumn(
-            modifier = Modifier.padding(top = padding.calculateTopPadding())
-        ) {
+        ProductList(Modifier.padding(top = padding.calculateTopPadding()))
+    }
+}
 
+@Composable
+private fun ProductList(modifier: Modifier){
+    var searchField by remember { mutableStateOf("") }
+
+    LazyColumn(
+        modifier = modifier
+            .padding(vertical = 14.dp, horizontal = 14.dp)
+    ) {
+        item {
+            OutlinedTextField(
+                value = searchField,
+                onValueChange = {
+                    searchField = it
+                    Log.d("Seach", searchField)
+                },
+                label = {Text("Поиск")},
+                modifier = Modifier.fillMaxWidth()
+            )
         }
-        Text(text = "")
     }
 }
